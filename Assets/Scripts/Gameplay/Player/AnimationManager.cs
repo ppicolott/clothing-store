fc0825d87e1d6fb8a2c2b4b8a475f3136b5c3f71
Paula Picolott <paula_picolott@hotmail.com>
2023-01-10 01:29:09 -0300

    Adding movement and synchronizing body, hair and clothes animations

---
+++ Assets/Scripts/Gameplay/Player/AnimationManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    public GameObject frontIdle, rightIdle, leftIdle, backIdle;
+
+    public GameObject frontWalk, rightWalk, leftWalk, backWalk;
+}
--- Assets/Scripts/Gameplay/Player/Movement.cs
+++ Assets/Scripts/Gameplay/Player/Movement.cs
@@ -1,8 +1,179 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    //
+    [SerializeField]
+    private Rigidbody2D rigidBody;
+
+    [SerializeField]
+    private AnimationManager[] animations;
+
+    [SerializeField]
+    private float moveSpeed = 3f;
+
+    private Movement singleInstance;
+    public static Movement instance;
+
+    private void LoadAnimationList()
+    {
+        animations = GetComponentsInChildren<AnimationManager>();
+    }
+    private void Awake()
+    {
+        instance = this;
+        LoadAnimationList();
+    }
+
+    private void FixedUpdate()
+    {
+        // Updating animations list when changing clothes
+        foreach (AnimationManager anim in animations)
+        {
+            if (!anim)
+            {
+                LoadAnimationList();
+            }
+        }
+
+        if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
+        {
+            // Move
+            rigidBody.velocity = new Vector2(0f, -moveSpeed);
+
+            foreach (AnimationManager anim in animations)
+            {
+                // Idle
+                anim.frontIdle.gameObject.SetActive(false);
+                anim.rightIdle.gameObject.SetActive(false);
+                anim.leftIdle.gameObject.SetActive(false);
+                anim.backIdle.gameObject.SetActive(false);
+                // Walk
+                anim.frontWalk.gameObject.SetActive(true);
+                anim.rightWalk.gameObject.SetActive(false);
+                anim.leftWalk.gameObject.SetActive(false);
+                anim.backWalk.gameObject.SetActive(false);
+            }
+        }
+        else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+        {
+            // Move
+            rigidBody.velocity = new Vector2(moveSpeed, 0f);
+
+            foreach (AnimationManager anim in animations)
+            {
+                // Idle
+                anim.frontIdle.gameObject.SetActive(false);
+                anim.rightIdle.gameObject.SetActive(false);
+                anim.leftIdle.gameObject.SetActive(false);
+                anim.backIdle.gameObject.SetActive(false);
+                // Walk
+                anim.frontWalk.gameObject.SetActive(false);
+                anim.rightWalk.gameObject.SetActive(true);
+                anim.leftWalk.gameObject.SetActive(false);
+                anim.backWalk.gameObject.SetActive(false);
+            }
+        }
+        else if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+        {
+            // Move
+            rigidBody.velocity = new Vector2(-moveSpeed, 0f);
+
+            foreach (AnimationManager anim in animations)
+            {
+                // Idle
+                anim.frontIdle.gameObject.SetActive(false);
+                anim.rightIdle.gameObject.SetActive(false);
+                anim.leftIdle.gameObject.SetActive(false);
+                anim.backIdle.gameObject.SetActive(false);
+                // Walk
+                anim.frontWalk.gameObject.SetActive(false);
+                anim.rightWalk.gameObject.SetActive(false);
+                anim.leftWalk.gameObject.SetActive(true);
+                anim.backWalk.gameObject.SetActive(false);
+            }
+        }
+        else if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
+        {
+            // Move
+            rigidBody.velocity = new Vector2(0f, moveSpeed);
+
+            foreach (AnimationManager anim in animations)
+            {
+                // Idle
+                anim.frontIdle.gameObject.SetActive(false);
+                anim.rightIdle.gameObject.SetActive(false);
+                anim.leftIdle.gameObject.SetActive(false);
+                anim.backIdle.gameObject.SetActive(false);
+                // Walk
+                anim.frontWalk.gameObject.SetActive(false);
+                anim.rightWalk.gameObject.SetActive(false);
+                anim.leftWalk.gameObject.SetActive(false);
+                anim.backWalk.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            // Stop
+            rigidBody.velocity = Vector3.zero;
+
+            foreach (AnimationManager anim in animations)
+            {
+                if (anim.frontWalk.gameObject.activeInHierarchy)
+                {
+                    // Idle
+                    anim.frontIdle.gameObject.SetActive(true);
+                    anim.rightIdle.gameObject.SetActive(false);
+                    anim.leftIdle.gameObject.SetActive(false);
+                    anim.backIdle.gameObject.SetActive(false);
+                    // Walk
+                    anim.frontWalk.gameObject.SetActive(false);
+                    anim.rightWalk.gameObject.SetActive(false);
+                    anim.leftWalk.gameObject.SetActive(false);
+                    anim.backWalk.gameObject.SetActive(false);
+                }
+                else if (anim.rightWalk.gameObject.activeInHierarchy)
+                {
+                    // Idle
+                    anim.frontIdle.gameObject.SetActive(false);
+                    anim.rightIdle.gameObject.SetActive(true);
+                    anim.leftIdle.gameObject.SetActive(false);
+                    anim.backIdle.gameObject.SetActive(false);
+                    // Walk
+                    anim.frontWalk.gameObject.SetActive(false);
+                    anim.rightWalk.gameObject.SetActive(false);
+                    anim.leftWalk.gameObject.SetActive(false);
+                    anim.backWalk.gameObject.SetActive(false);
+                }
+                else if (anim.leftWalk.gameObject.activeInHierarchy)
+                {
+                    // Idle
+                    anim.frontIdle.gameObject.SetActive(false);
+                    anim.rightIdle.gameObject.SetActive(false);
+                    anim.leftIdle.gameObject.SetActive(true);
+                    anim.backIdle.gameObject.SetActive(false);
+                    // Walk
+                    anim.frontWalk.gameObject.SetActive(false);
+                    anim.rightWalk.gameObject.SetActive(false);
+                    anim.leftWalk.gameObject.SetActive(false);
+                    anim.backWalk.gameObject.SetActive(false);
+                }
+                else if (anim.backWalk.gameObject.activeInHierarchy)
+                {
+                    // Idle
+                    anim.frontIdle.gameObject.SetActive(false);
+                    anim.rightIdle.gameObject.SetActive(false);
+                    anim.leftIdle.gameObject.SetActive(false);
+                    anim.backIdle.gameObject.SetActive(true);
+                    // Walk
+                    anim.frontWalk.gameObject.SetActive(false);
+                    anim.rightWalk.gameObject.SetActive(false);
+                    anim.leftWalk.gameObject.SetActive(false);
+                    anim.backWalk.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
 }
